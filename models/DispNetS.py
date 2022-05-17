aaf133eabebe80d1768620f5163bc37122df190d
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-05-17 17:58:52 -0400

    Added conv layers

--- models/DispNetS.py
+++ models/DispNetS.py
@@ -7,8 +7,10 @@ from torch.nn.init import xavier_uniform_, zeros_
 def downsample_conv(in_planes, out_planes, kernel_size=3):
     return nn.Sequential(
         nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=2, padding=(kernel_size-1)//2),
+        nn.BatchNorm2d(out_planes),
         nn.ReLU(inplace=True),
         nn.Conv2d(out_planes, out_planes, kernel_size=kernel_size, padding=(kernel_size-1)//2),
+        nn.BatchNorm2d(out_planes),
         nn.ReLU(inplace=True)
     )
 
@@ -23,6 +25,7 @@ def predict_disp(in_planes):
 def conv(in_planes, out_planes):
     return nn.Sequential(
         nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1),
+        nn.BatchNorm2d(out_planes),
         nn.ReLU(inplace=True)
     )
 
@@ -30,6 +33,7 @@ def conv(in_planes, out_planes):
 def upconv(in_planes, out_planes):
     return nn.Sequential(
         nn.ConvTranspose2d(in_planes, out_planes, kernel_size=3, stride=2, padding=1, output_padding=1),
+        nn.BatchNorm2d(out_planes),
         nn.ReLU(inplace=True)
     )
 
@@ -42,7 +46,7 @@ def crop_like(input, ref):
 class DispNetS(nn.Module):
 
     def __init__(self, alpha=10, beta=0.01):
-        super(DispNetS, self).__init__()
+        super(DispNetS2, self).__init__()
 
         self.alpha = alpha
         self.beta = beta
@@ -55,6 +59,13 @@ class DispNetS(nn.Module):
         self.conv5 = downsample_conv(conv_planes[3], conv_planes[4])
         self.conv6 = downsample_conv(conv_planes[4], conv_planes[5])
         self.conv7 = downsample_conv(conv_planes[5], conv_planes[6])
+
+        self.conv7_1x1 = nn.Conv2d(conv_planes[5], conv_planes[5], kernel_size=1, padding=0)
+        self.conv6_1x1 = nn.Conv2d(conv_planes[4], conv_planes[4], kernel_size=1, padding=0)
+        self.conv5_1x1 = nn.Conv2d(conv_planes[3], conv_planes[3], kernel_size=1, padding=0)
+        self.conv4_1x1 = nn.Conv2d(conv_planes[2], conv_planes[2], kernel_size=1, padding=0)
+        self.conv3_1x1 = nn.Conv2d(conv_planes[1], conv_planes[1], kernel_size=1, padding=0)
+        self.conv2_1x1 = nn.Conv2d(conv_planes[0], conv_planes[0], kernel_size=1, padding=0)
 
         upconv_planes = [512, 512, 256, 128, 64, 32, 16]
         self.upconv7 = upconv(conv_planes[6],   upconv_planes[0])
@@ -95,30 +106,36 @@ class DispNetS(nn.Module):
         out_conv7 = self.conv7(out_conv6)
 
         out_upconv7 = crop_like(self.upconv7(out_conv7), out_conv6)
+        out_upconv7 = self.conv7_1x1(out_upconv7)
         concat7 = torch.cat((out_upconv7, out_conv6), 1)
         out_iconv7 = self.iconv7(concat7)
 
         out_upconv6 = crop_like(self.upconv6(out_iconv7), out_conv5)
+        out_upconv6 = self.conv6_1x1(out_upconv6)
         concat6 = torch.cat((out_upconv6, out_conv5), 1)
         out_iconv6 = self.iconv6(concat6)
 
         out_upconv5 = crop_like(self.upconv5(out_iconv6), out_conv4)
+        out_upconv5 = self.conv5_1x1(out_upconv5)
         concat5 = torch.cat((out_upconv5, out_conv4), 1)
         out_iconv5 = self.iconv5(concat5)
 
         out_upconv4 = crop_like(self.upconv4(out_iconv5), out_conv3)
+        out_upconv4 = self.conv4_1x1(out_upconv4)
         concat4 = torch.cat((out_upconv4, out_conv3), 1)
         out_iconv4 = self.iconv4(concat4)
         disp4 = self.alpha * self.predict_disp4(out_iconv4) + self.beta
 
         out_upconv3 = crop_like(self.upconv3(out_iconv4), out_conv2)
         disp4_up = crop_like(F.interpolate(disp4, scale_factor=2, mode='bilinear', align_corners=False), out_conv2)
+        out_upconv3 = self.conv3_1x1(out_upconv3)
         concat3 = torch.cat((out_upconv3, out_conv2, disp4_up), 1)
         out_iconv3 = self.iconv3(concat3)
         disp3 = self.alpha * self.predict_disp3(out_iconv3) + self.beta
 
         out_upconv2 = crop_like(self.upconv2(out_iconv3), out_conv1)
         disp3_up = crop_like(F.interpolate(disp3, scale_factor=2, mode='bilinear', align_corners=False), out_conv1)
+        out_upconv2 = self.conv2_1x1(out_upconv2)
         concat2 = torch.cat((out_upconv2, out_conv1, disp3_up), 1)
         out_iconv2 = self.iconv2(concat2)
         disp2 = self.alpha * self.predict_disp2(out_iconv2) + self.beta
